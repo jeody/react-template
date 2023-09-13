76c3da97a8a83f0eb68e0bb10d83d2a79fbe214e
Jhun Gulle <71265836+jeody@users.noreply.github.com>
2023-09-13 11:25:05 +0800

    react-starter-complete

---
+++ src/components/admin/home/Home.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Loader from '../../loader/Loader';
+
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [isLoading]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <h3>Dashboard ~</h3>
+          <p>
+            This is the user's <b>Dashboard Page</b>
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,26 @@
+import './Home.scss';
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+  //const name = 'John Doe';
+  const { name } = useSelector((state) => state.data);
+
+  return (
+    <div className='home'>
+      <div className='container'>
+        <h2>Home Page</h2>
+        <p>
+          Welcome to the <b>Home</b> page
+        </p>
+        <p>
+          This page has <b>Redux Toolkit</b> set up.
+        </p>
+        <p>
+          The name: <b>{name}</b> is stored in the dataSlice
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
